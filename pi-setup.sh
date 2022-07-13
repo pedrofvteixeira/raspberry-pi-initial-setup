896bd05759a98a6c577fd2e3de48b6ae1e382360
pedrofvteixeira <pedrofvteixeira@gmail.com>
2022-07-13 23:41:22 +0100

    minor fixes

--- pi-setup.sh
+++ pi-setup.sh
@@ -13,10 +13,12 @@ chmod +x /media/pedro/rootfs/home/pi/pi-first-boot.sh
 sudo mkdir -p /media/pedro/rootfs/etc/docker
 
 sudo cp -v daemon.json /media/pedro/rootfs/etc/docker/
-echo "" && cat /media/pedro/rootfs/etc/docker/daemon.json
+echo "[/etc/docker/daemon.json]"
+cat /media/pedro/rootfs/etc/docker/daemon.json
 
 sudo echo "${hostname}" > /media/pedro/rootfs/etc/hostname
-echo "" && cat /media/pedro/rootfs/etc/hostname
+echo "[/etc/hostname]"
+cat /media/pedro/rootfs/etc/hostname
 
 sudo echo "
 192.168.1.100   pi100
@@ -25,16 +27,15 @@ sudo echo "
 192.168.1.103   pi103
 192.168.1.104   pi104
 192.168.1.105   pi105
-
 " >> /media/pedro/rootfs/etc/hosts
-echo "" && cat /media/pedro/rootfs/etc/hosts
+echo "[/etc/hosts]"
+cat /media/pedro/rootfs/etc/hosts
 
 sudo echo "
-
 interface wlan0
 static ip_address=192.168.1.${hostname}
 static_routers=192.168.1.1
 static domain_name_servers=192.168.1.1 1.1.1.1
-
 " >> /media/pedro/rootfs/etc/dhcpcd.conf
-echo "" && cat /media/pedro/rootfs/etc/dhcpcd.conf
+echo "[/etc/dhcpcd.conf]"
+cat /media/pedro/rootfs/etc/dhcpcd.conf
