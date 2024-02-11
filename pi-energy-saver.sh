3f72bb15169ee1407a4455a7bf607177f2780177
Pedro Teixeira <pedrofvteixeira@gmail.com>
2024-02-11 20:23:28 +0000

    Update pi-energy-saver.sh

--- pi-energy-saver.sh
+++ pi-energy-saver.sh
@@ -1,32 +1,21 @@
 #!/bin/sh
 
 ###
-# https://scribles.net/disabling-bluetooth-on-raspberry-pi/
-# https://www.raspberrypi.org/forums/viewtopic.php?p=1286812
-# https://stackoverflow.com/questions/23487728/ethernet-disabling-in-raspberry-pi#37952479
-# https://thomas.vanhoutte.be/miniblog/disable-the-red-and-green-lights-on-a-raspberry-pi/
-##
+# https://n.ethz.ch/~dbernhard/disable-led-on-a-raspberry-pi.html
+###
+echo "
+[Unit]
+Description=Disables the power-LED and active-LED
+After=multi-user.target
 
-ENABLED=1
-DISABLED=0
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=sh -c 'echo 1 | sudo tee /sys/class/leds/PWR/brightness > /dev/null && echo 0 | sudo tee /sys/class/leds/ACT/brightness > /dev/null'
+ExecStop=sh -c 'echo 0 | sudo tee /sys/class/leds/PWR/brightness > /dev/null && echo 1 | sudo tee /sys/class/leds/ACT/brightness > /dev/null'
 
-CHOICE="$DISABLED"
-
-BLUETOOTH_OVERLAY="pi3-disable-bt"
-if [ "${CHOICE}" = "${ENABLED}" ]; then BLUETOOTH_OVERLAY="pi3-enable-bt"; fi	
-
-echo "-- start --"
-
-echo "-- setting green (activity) light to ${CHOICE} --"
-echo ${CHOICE} > /sys/class/leds/led0/brightness
-
-echo "-- setting red (power) light to ${CHOICE} --"
-echo ${CHOICE} > /sys/class/leds/led1/brightness
-
-echo "-- setting ethernet+usb chip's power draw to ${CHOICE} --"
-echo ${CHOICE} > /sys/devices/platform/soc/3f980000.usb/buspower
-
-echo "-- setting bluetooth to ${BLUETOOTH_OVERLAY} --"
-echo "dtoverlay=${BLUETOOTH_OVERLAY}" >> /boot/config.txt
+[Install]
+WantedBy=multi-user.target
+" >> /etc/systemd/system/disable-leds.service
 
 echo "-- done --"
